e6130c349416073699e80c278390e92bc30a7dfb
AldoMM365 <84868108+AldoMM365@users.noreply.github.com>
2025-10-03 17:57:39 -0600

    Added assignment operator overcharge

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,6 +1,7 @@
 #include "Matrix.h"
 
 #include <cstdlib>
+#include <stdexcept>
 
 Matrix::Matrix(int rows, int cols) {
     this->rows = rows;
@@ -21,4 +22,23 @@ Matrix::~Matrix() {
 
 int & Matrix::get(int row, int col) {
     return matrix[row][col];
+}
+
+Matrix & Matrix::operator=(std::initializer_list<std::initializer_list<int>> list){
+    int rowIndex = 0;
+    int columnIndex = 0;
+    if (list.size() != rows)
+        throw std::invalid_argument("Provided brace-enclosed initializer list does not match current numer of rows");
+
+    for (std::initializer_list<int> row : list) {
+        if (row.size() != cols)
+                throw std::invalid_argument("Provided brace-enclosed initializer list does not match the current numer of columns at row " + std::to_string(rowIndex));
+        columnIndex = 0;
+        for (int col: row) {
+            matrix[rowIndex][columnIndex] = col;
+            columnIndex++;
+        }
+        rowIndex++;
+    }
+    return *this;
 }
--- Matrix.h
+++ Matrix.h
@@ -1,11 +1,13 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+#include <initializer_list>
 
 class Matrix {
 public:
     Matrix(int rows, int cols);
     ~Matrix();
     int & get(int row, int col);
+    Matrix& operator=(std::initializer_list<std::initializer_list<int>> list);
 private:
     int rows;
     int cols;
